5485c2f61c5c9ee7041f2e6417a16877071ee6df
abubakarbozon <abubakaraminuusman46@gmail.com>
2026-06-20 23:53:44 +0100

    email

--- src/email.js
+++ src/email.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const resend = process.env.RESEND_API_KEY ? new Resend(process.env.RESEND_API_KEY) : null;
 
-const FROM_EMAIL = process.env.RESEND_FROM_EMAIL || 'BuyNear <onboarding@resend.dev>';
+const FROM_EMAIL = process.env.RESEND_FROM_EMAIL;
 const APP_NAME   = 'BuyNear';
 
 /**
